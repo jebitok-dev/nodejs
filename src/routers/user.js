7816dbe5d07d89fc506d8434aebec6ddcbc4b3be
jebitok-dev <sharonkosgei4@gmail.com>
2020-02-26 16:37:11 +0300

    logOut & logIn auth

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,13 +8,15 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token});
     } catch(e) {
         res.status(400).send(e)
     }
 });
 
 router.post('/users/login', async(req, res) => {
+
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthToken();
@@ -24,13 +26,20 @@ router.post('/users/login', async(req, res) => {
     }
 })
 
-router.get('/users', auth, async (req, res) => {
+router.post('/users/logout', auth, async(req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
-    }catch (e) {
+        req.user.tokens = req.user.tokens.filter((token) => {
+        return token.token !== req.token; 
+    })
+    await req.user.save();
+    res.send();
+    } catch (e) {
         res.status(500).send(e)
     }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 });
 
 router.get ('/users/:id', async (req, res) => {
