d090bbf2f920b0a8db1fd5e7534b494e6c211875
jebitok-dev <sharonkosgei4@gmail.com>
2020-02-28 09:39:38 +0300

    local commit #4

---
+++ src/playground/objectKeys.js
@@ -0,0 +1,11 @@
+var creds = {name: "Kathy", city: 'berlin', job: "js girl"}
+console.log(Object.keys(creds))
+
+var creds = {"name": "Kathy", "city": 'berlin', "job": "js girl"}
+
+console.log(creds['job']);
+console.log(creds['name']);
+console.log(creds['city']);
+
+
+
