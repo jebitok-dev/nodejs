5075f9c5d022842bb835528406541b0e9036b786
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-07 18:28:54 +0300

    Add Append & Write File

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const importedStuff = require('./users');
-console.log(importedStuff);
+const fs = require('fs')
+// Read File
+// fs.readFile('./ckmobile/note.txt', (err, data) => {
+//     if(err) {
+//         console.log(err)
+//     } else {
+//         console.log(data.toString())
+//     }
+// })
+
+// Write File
+fs.writeFile('./ckmobile/note.txt', 'A new message', (err, data) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('A new message created')
+    }
+})
