00c70c662d98f724e29fe8485cc14a7b73be263e
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-07 18:50:50 +0300

    Add Created Folder

--- app.js
+++ app.js
@@ -17,10 +17,26 @@ const fs = require('fs')
 //     }
 // })
 
-fs.appendFile('./ckmobile/note.txt', 'A new Message 2\r\n', (err, data) => {
-    if(err) {
-        console.log(err)
-    } else {
-        console.log('A new message created')
-    }
-})
+//
+
+// fs.appendFile('./ckmobile/note.txt', 'A new Message 2\r\n', (err, data) => {
+//     if(err) {
+//         console.log(err)
+//     } else {
+//         console.log('A new message created')
+//     }
+// })
+
+// create folder
+
+if(!fs.existsSync('newfolder')) {
+    fs.mkdir('new folder', (err, data) => {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log('A new folder created')
+        }
+    })
+} else {
+    console.log('folder already exists')
+}
