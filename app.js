0d1b964e0e9a2abaf1c86b3690a6f6b25d9c7647
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-07 18:53:57 +0300

    Delete Folder

--- app.js
+++ app.js
@@ -29,14 +29,27 @@ const fs = require('fs')
 
 // create folder
 
+// if(!fs.existsSync('newfolder')) {
+//     fs.mkdir('new folder', (err, data) => {
+//         if(err) {
+//             console.log(err)
+//         } else {
+//             console.log('A new folder created')
+//         }
+//     })
+// } else {
+//     console.log('folder already exists')
+// }
+
+// delete folder
 if(!fs.existsSync('newfolder')) {
-    fs.mkdir('new folder', (err, data) => {
+    fs.rmdir('new folder', (err, data) => {
         if(err) {
             console.log(err)
         } else {
-            console.log('A new folder created')
+            console.log('folder deleted')
         }
     })
 } else {
-    console.log('folder already exists')
+    console.log('folder doesnt exists')
 }
