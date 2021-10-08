7f26ccd74e129a6eeabeeb0c21d81854e543e199
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 18:25:13 +0300

    Fix Linters

--- ckmobile/server.js
+++ ckmobile/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const moment = require('moment');
-const hostname = 'localhost';
+// const hostname = 'localhost';
 const port = 8080;
 const christmas = '2022-12-25';
 console.log(moment(christmas).format('LL'));
@@ -30,7 +30,7 @@ const server = http.createServer((req, res) => {
             res.statusCode = 404;
             break;
     }
-
+/* eslint-disable*/ 
    fs.readFile(route, (err, data) => {
        if(err) {
            console.log(err)
