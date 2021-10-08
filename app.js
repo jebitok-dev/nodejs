288669fddec95beeaf6cf49fbdb759f5751c113f
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 04:46:13 +0300

    Add Readstream

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const fs = require('fs')
 const readStream = fs.createReadStream('largetext.txt')
 readStream.on('data', chunk => {
     console.log('### new chunk ###')
-    console.log(chunk)
+    console.log(chunk.toString())
 }) 
