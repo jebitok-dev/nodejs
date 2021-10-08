5c75ee6349f32e8c13ce02179aca06031a0f078f
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 04:45:45 +0300

    Add Readstream

--- app.js
+++ app.js
@@ -1,69 +1,7 @@
 const fs = require('fs')
-// Read File
-// fs.readFile('./ckmobile/note.txt', (err, data) => {
-//     if(err) {
-//         console.log(err)
-//     } else {
-//         console.log(data.toString())
-//     }
-// })
 
-// Write File
-// fs.writeFile('./ckmobile/note.txt', 'A new message', (err, data) => {
-//     if(err) {
-//         console.log(err)
-//     } else {
-//         console.log('A new message created')
-//     }
-// })
-
-//
-
-// fs.appendFile('./ckmobile/note.txt', 'A new Message 2\r\n', (err, data) => {
-//     if(err) {
-//         console.log(err)
-//     } else {
-//         console.log('A new message created')
-//     }
-// })
-
-// create folder
-
-// if(!fs.existsSync('newfolder')) {
-//     fs.mkdir('new folder', (err, data) => {
-//         if(err) {
-//             console.log(err)
-//         } else {
-//             console.log('A new folder created')
-//         }
-//     })
-// } else {
-//     console.log('folder already exists')
-// }
-
-// delete folder
-// if(!fs.existsSync('newfolder')) {
-//     fs.rmdir('new folder', (err, data) => {
-//         if(err) {
-//             console.log(err)
-//         } else {
-//             console.log('folder deleted')
-//         }
-//     })
-// } else {
-//     console.log('folder doesnt exists')
-// }
-
-// Delete File
-
-if(!fs.existsSync('newfolder')) {
-    fs.unlink('./ckmobile/note.txt', (err, data) => {
-        if(err) {
-            console.log(err)
-        } else {
-            console.log('The file is deleted')
-        }
-    })
-} else {
-    console.log('folder already exists')
-}
+const readStream = fs.createReadStream('largetext.txt')
+readStream.on('data', chunk => {
+    console.log('### new chunk ###')
+    console.log(chunk)
+}) 
