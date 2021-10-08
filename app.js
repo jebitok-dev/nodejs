7288bf5127021cd2769889bc6dc854a779093d5e
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 04:53:22 +0300

    Add Stream.pipe

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const fs = require('fs')
 
 const readStream = fs.createReadStream('largetext.txt', {encoding: 'utf-8'});
 const writeStream = fs.createWriteStream('writeStream.txt');
-readStream.on('data', chunk => {
-    console.log('### new chunk ###')
-    console.log(chunk)
+// readStream.on('data', chunk => {
+//     console.log('### new chunk ###')
+//     console.log(chunk)
 
-    writeStream.write('\n ####new chunk#####\n');
-    writeStream.write(chunk)
-}) 
+//     writeStream.write('\n ####new chunk#####\n');
+//     writeStream.write(chunk)
+// }) 
+// stream.pipe()
+readStream.pipe(writeStream)
