53199ddbb924ec6448ec7baaccc80008c2623cb2
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 05:20:59 +0300

    Add Body

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const hostname = 'localhost';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-   console.log('request url', req.url) 
-   console.log('request method', req.method);
+   res.setHeader('Content-Type', 'text/html');
+   res.write('<body class="app"></body>');
+   res.write('<h1>Welcome to ckmobile</h3>');
+   res.write('<p>This is Node.js course</p>');
+   res.end();
 })
 
 server.listen(port, () => {
