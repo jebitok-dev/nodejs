aed7db90bfb2c6b61d20a35e6f3f1c3b92bf08e5
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 05:26:30 +0300

    Add HTML File

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 const hostname = 'localhost';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
    res.setHeader('Content-Type', 'text/html');
-   res.write('<body class="app"></body>');
-   res.write('<h1>Welcome to ckmobile</h3>');
-   res.write('<p>This is Node.js course</p>');
-   res.end();
-})
+   fs.readFile('./view/index.html', (err, data) => {
+       if(err) {
+           console.log(err)
+           res.end()
+       } else {
+           res.write(data);
+           res.end();
+       }
+    })
+});
 
 server.listen(port, () => {
     console.log(`listening on port ${port}`)
