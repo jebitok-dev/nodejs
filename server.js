947489daacc7203446c02525dd836e9c9c07f3ad
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 05:37:50 +0300

    Add Routes

--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const port = 8080;
 
 const server = http.createServer((req, res) => {
    res.setHeader('Content-Type', 'text/html');
-   fs.readFile('./view/index.html', (err, data) => {
+
+   let route = "./views/"
+
+    switch(req.url) {
+        case '/':
+            route += 'index.html'
+            break;
+        case '/contact':
+            route += 'contact.html'
+            break;
+        default:
+            route += '404.html'
+            break;
+    }
+
+   fs.readFile(route, (err, data) => {
        if(err) {
            console.log(err)
            res.end()
