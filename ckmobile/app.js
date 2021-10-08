ee2580616cc1863844676e7c38d98bbb9db0cfd6
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-08 17:56:04 +0300

    Finalize Nodejs

--- ckmobile/app.js
+++ ckmobile/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World!')
+    res.send('<h1>Welcome to CkMobile!</h1>')
 })
 
 app.listen(3000)
