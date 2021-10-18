cabb2a6efc236c3ecfa02cdf6732d10a36f82dfe
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-18 11:44:13 +0300

    Add Changes to EJS File

--- express-app/app.js
+++ express-app/app.js
@@ -5,7 +5,12 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index', {title2: 'some title2'})
+    const items = [
+        {name: 'mobile phone', price: 1000},
+        {name: 'book', price: 30},
+        {name: 'computer', price: 2000},
+    ]
+    res.render('index', {items})
 })
 
 app.get('/add-item', (req, res) => {
