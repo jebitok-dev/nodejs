3770b0834be21bfe1772dad58d9804d4925bcb11
jebitok-dev <sharonkosgei4@gmail.com>
2021-11-03 15:48:29 +0300

    Add Read Query

--- Mern-Todo/playground/index.js
+++ Mern-Todo/playground/index.js
@@ -1,8 +1,6 @@
 var http = require('http');
-var dt = require('./date');
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
+  res.end();
 }).listen(8080);
